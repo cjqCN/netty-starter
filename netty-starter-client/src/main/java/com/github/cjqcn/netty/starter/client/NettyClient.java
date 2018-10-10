acc066b703eb4ea663e3c195b8c49309997ba7cd
chenjinquan <chenjinquan@banggood.com>
2018-10-10 16:31:21 +0800

    Update

--- netty-starter-client/src/main/java/com/github/cjqcn/netty/starter/client/NettyClient.java
+++ netty-starter-client/src/main/java/com/github/cjqcn/netty/starter/client/NettyClient.java
@@ -1,9 +1,7 @@
 package com.github.cjqcn.netty.starter.client;
 
-
 import com.github.cjqcn.netty.starter.common.Req;
 import com.github.cjqcn.netty.starter.common.Sender;
-import com.github.cjqcn.netty.starter.common.req.HelloReq;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelOption;
@@ -17,62 +15,77 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+
 /**
  * @description:
  * @author: chenjinquan
  * @create: 2018-09-25 14:41
  **/
-public class NettyClient implements Runnable {
+public class NettyClient implements Runnable, Sender<Req> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(NettyClient.class);
 
-    static final String HOST = System.getProperty("host", "127.0.0.1");
-    static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
-    ScheduledExecutorService service;
-    volatile Sender sender;
+	private static final Logger LOG = LoggerFactory.getLogger(NettyClient.class);
 
-    public synchronized void start() throws Exception {
-        service = Executors.newScheduledThreadPool(1);
-        service.scheduleAtFixedRate(this, 0, 10, TimeUnit.SECONDS);
-    }
+	static final String HOST = System.getProperty("host", "127.0.0.1");
+	static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
+	private ScheduledExecutorService service;
+	private volatile Sender sender;
+	private boolean isActive;
 
-    public static void main(String[] args) throws Exception {
-        NettyClient client = new NettyClient();
-        client.start();
-        Thread.sleep(1000L);
-        client.sender().send(new HelloReq());
-    }
+	private NettyClient() {
+	}
 
-    @Override
-    public void run() {
-        // Configure the client.
-        LOG.info("正在连接服务器----");
-        Bootstrap b = new Bootstrap();
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-        try {
-            b.group(workerGroup)
-                    .channel(NioSocketChannel.class)
-                    .option(ChannelOption.TCP_NODELAY, true)
-                    .handler(new NettyChannelInitializer());
+	public static NettyClient instance = new NettyClient();
 
-            // Start the client.
-            ChannelFuture f = b.connect(HOST, PORT).sync();
-            LOG.info("连接服务器成功----");
-            // Init sender
-            ClientHandler clientHandler = (ClientHandler) f.channel().pipeline().get("clientHandler");
-            sender = clientHandler;
+	public synchronized void start() {
+		if (isActive == false) {
+			isActive = true;
+			service = Executors.newScheduledThreadPool(1);
+			service.scheduleAtFixedRate(this, 0, 10, TimeUnit.SECONDS);
+		}
+	}
 
-            // Wait until the connection is closed.
-            f.channel().closeFuture().sync();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // Shut down the event loop to terminate all threads.
-            workerGroup.shutdownGracefully();
-        }
-    }
+	@Override
+	public void run() {
+		// Configure the client.
+		LOG.info("正在连接服务器----");
+		Bootstrap b = new Bootstrap();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			b.group(workerGroup)
+					.channel(NioSocketChannel.class)
+					.option(ChannelOption.TCP_NODELAY, true)
+					.handler(new NettyChannelInitializer());
 
-    public Sender<Req> sender() {
-        return sender;
-    }
+			// Start the client.
+			ChannelFuture f = b.connect(HOST, PORT).sync();
+			LOG.info("连接服务器成功----");
+			// Init sender
+			ClientHandler producerHandler = (ClientHandler) f.channel().pipeline().get("clientHandler");
+			sender = producerHandler;
+
+			// Wait until the connection is closed.
+			f.channel().closeFuture().sync();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// Shut down the event loop to terminate all threads.
+			workerGroup.shutdownGracefully();
+		}
+	}
+
+
+	/**
+	 * 发送信息
+	 *
+	 * @param req
+	 */
+	@Override
+	public void send(Req req) throws Exception {
+		if (sender == null) {
+			throw new RuntimeException("连接 admin 失败");
+		}
+		sender.send(req);
+	}
+
 }
